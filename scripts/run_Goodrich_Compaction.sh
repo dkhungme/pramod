cc725d74a944ca9fc73ad772ee2866d2b50898b7
khanhhung1410 <khanhhung1410@github.com>
2015-05-06 14:31:52 +0800

    experiments

--- scripts/run_Goodrich_Compaction.sh
+++ scripts/run_Goodrich_Compaction.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 NBLOCKS=(16 32 64 128 256) 
+M=(131072 262144 393216 524288)
 DIR=`pwd`
 CONFIG=../config
 LOG_DIR=logs
 mkdir -p ../$LOG_DIR
 
-for (( i=0; i<5; i++ )); do
-	cd $DIR
-	echo Compating ${NBLOCKS[i]}
+for (( i=0; i<4; i++ )); do
+	for (( j=0; j<5; j++ )); do
+		cd $DIR
+		echo Compating ${NBLOCKS[i]}
 
-	let INPUTSIZE=131072*${NBLOCKS[i]}
+		let INPUTSIZE=${M[i]}*${NBLOCKS[j]}
 
-        sed -e "s/num_records_per_block:.*/num_records_per_block:$INPUTSIZE/g" $CONFIG >tmp1
-	mv tmp1 $CONFIG
+		sed -e "s/num_records_per_block:.*/num_records_per_block:$INPUTSIZE/g" $CONFIG >tmp1
+		mv tmp1 $CONFIG
 
-	sed -e "s/num_records:.*/num_records:$INPUTSIZE/g" $CONFIG >tmp1
-	mv tmp1 $CONFIG
+		sed -e "s/num_records:.*/num_records:$INPUTSIZE/g" $CONFIG >tmp1
+		mv tmp1 $CONFIG
 	
-	cd ../
-	rm -rf data/*
-	build/test/Goodrich_Compact_datagen
-	sleep 10
-	COMMAND="build/test/goodrich_compact> $LOG_DIR/goodrich_Compact_N131072x${NBLOCKS[i]}_B512 2>&1"
-	eval $COMMAND
+		cd ../
+		rm -rf data/*
+		build/test/Goodrich_Compact_datagen
+		sleep 10
+		COMMAND="build/test/goodrich_compact> $LOG_DIR/goodrich_Compact_N${M[i]}x${NBLOCKS[j]}_B1024 2>&1"
+		eval $COMMAND
 
-	sleep 5
+		sleep 5
+	done
 done
 rm -rf tmp1
